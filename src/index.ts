6b3c5e5fc0d77d9330cb54fb0858e0ad46f5405d
remvst <remvst@gmail.com>
2024-08-30 01:32:42 -0400

    Whoopsies

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ export * from './assemble-html';
 export * from './hardcode-constants';
 export * from './log-file-size';
 export * from './macro';
-export * from './make-zip';
 export * from './mangle';
 
 export * from './macros/evaluate';
